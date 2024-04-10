8f138b550346ce6197500b46d064deefd2fd168e
quantwav <raven_wann_uibe@126.com>
2024-04-10 16:56:43 -0400

    wrz

--- Factor/__init__.py
+++ Factor/__init__.py
@@ -1,4 +1,5 @@
 from factors import *
 FACTORS = {
-    'wap1':wap1
+    'wap1':wap1,
+    'wap2':wap2,
 }
--- Factor/factors.py
+++ Factor/factors.py
@@ -6,3 +6,11 @@ def wap1(df:pd.DataFrame) -> pd.Series:
         return wap
     df['wap1'] = calc_wap1(df)
     return df
+
+def wap2(df:pd.DataFrame) -> pd.Series:
+    def calc_wap2(df: pd.DataFrame) -> pd.Series:
+        wap = (df['bid_price2'] * df['ask_size2'] + df['ask_price2'] * df['bid_size2']) / (df['bid_size2'] + df['ask_size2'])
+        return wap
+    df['wap2'] = calc_wap2(df)
+    return df
+
