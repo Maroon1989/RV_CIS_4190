4816266fded370be32c093578415fc541f216b01
Maroon1989 <2803666041@qq.com>
2024-05-10 23:59:52 +0800

    some modification

--- Factor/__init__.py
+++ Factor/__init__.py
@@ -1,5 +1,14 @@
 from factors import *
 FACTORS = {
-    'wap1':wap1,
-    'wap2':wap2,
+    'wap1':[wap1,[np.mean,np.sum,np.std]],
+    'wap2':[wap2,[np.mean,np.sum,np.std]],
+    'rtn5_mean':[rtn5_mean,None],
+    'real_var':[real_var,None]
+    # 'real_skew':[real_skew,None],
+    # 'real_kurt':[real_kurt,None],
+    # 'rv_up':[rv_up,None],
+    # 'rv_down':[rv_down,None],
+    # 'nog_gs':[nog_gs,None],
+    # 'exRtn_maxVal':[exRtn_maxVal,None],
+    # 'exRtn_minFre':[exRtn_minFre,None]
 }
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOOK_PATH = r'D:\CIS 4190 project\data\optiver-realized-volatility-prediction (1)\book_train.parquet'
-TRADE_PATH = r'D:\CIS 4190 project\data\optiver-realized-volatility-prediction (1)\trade_train.parquet'
+BOOK_PATH = r'E:\BaiduNetdiskDownload\book_train.parquet'
+TRADE_PATH = r'E:\BaiduNetdiskDownload\trade_train.parquet'
 MEMORY_MODE = False
